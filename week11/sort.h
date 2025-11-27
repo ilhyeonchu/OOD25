28805b83ff9eb17ba95adebf8dbeb33a0db05a6d
ilhyeonchu <ilhyeonchu@gmail.com>
2025-11-27 23:20:34 +0900

    11 fin

--- week11/sort.h
+++ week11/sort.h
@@ -10,19 +10,21 @@ void Swap(T& elt1, T& elt2) {
 
 template <typename T, typename N>
 void Sort(typename T::iterator begin, typename T::iterator end) {
-  if (begin == end || begin == std::prev(end)) return;
+  if (begin == end || begin == std::prev(end)) {
+    return;
+  }
 
   N comp = N();
   typename T::iterator pivot = begin;
   typename T::iterator left = std::next(begin);
-  typename T::iterator i = std::next(begin);
+  typename T::iterator current = std::next(begin);
 
-  while (i != end) {
-    if (comp(*pivot, *i) == 1) {
-      Swap(*i, *left);
+  while (current != end) {
+    if (comp(*pivot, *current) == 1) {
+      Swap(*current, *left);
       left++;
     }
-    i++;
+    current++;
   }
   Swap(*pivot, *(--left));
   Sort<T, N>(begin, left);
--- week11/test.cc
+++ week11/test.cc
@@ -101,8 +101,8 @@ class SortTest : public ::testing::Test {
     }
   };
 
-  template <typename T>
-  std::string ContainerToString(const T& container) const {
+  template <typename Container>
+  std::string PrintContainer(const Container& container) const {
     std::ostringstream oss;
     bool first = true;
     for (const auto& n : container) {
@@ -129,26 +129,26 @@ TEST_F(IntSortTest, IntArrSortIncreasing) {
   this->arr = {1, 3, 5, 2, 6, 8, 4, 0};
   Sort<std::array<int, 8>, IntIncreasingOrder>(this->arr.begin(),
                                                this->arr.end());
-  EXPECT_EQ(this->ContainerToString(this->arr), "0 1 2 3 4 5 6 8");
+  EXPECT_EQ(this->PrintContainer(this->arr), "0 1 2 3 4 5 6 8");
 }
 
 TEST_F(IntSortTest, IntVectorSortIncreasing) {
   this->vec = {4, 2, 7, 1, 9, 3, 6, 5};
   Sort<std::vector<int>, IntIncreasingOrder>(this->vec.begin(),
                                              this->vec.end());
-  EXPECT_EQ(this->ContainerToString(this->vec), "1 2 3 4 5 6 7 9");
+  EXPECT_EQ(this->PrintContainer(this->vec), "1 2 3 4 5 6 7 9");
 }
 
 TEST_F(IntSortTest, IntListSortIncreasing) {
   this->lst = {8, 0, 5, 2, 7, 4, 6, 1};
   Sort<std::list<int>, IntIncreasingOrder>(this->lst.begin(), this->lst.end());
-  EXPECT_EQ(this->ContainerToString(this->lst), "0 1 2 4 5 6 7 8");
+  EXPECT_EQ(this->PrintContainer(this->lst), "0 1 2 4 5 6 7 8");
 }
 
 TEST_F(IntSortTest, IntDequeSortIncreasing) {
   this->deq = {3, 3, 1, 2, 6, 5, 4, 0};
   Sort<std::deque<int>, IntIncreasingOrder>(this->deq.begin(), this->deq.end());
-  EXPECT_EQ(this->ContainerToString(this->deq), "0 1 2 3 3 4 5 6");
+  EXPECT_EQ(this->PrintContainer(this->deq), "0 1 2 3 3 4 5 6");
 }
 
 using FloatSortTest = SortTest<float>;
@@ -157,28 +157,28 @@ TEST_F(FloatSortTest, FloatArrSortDecreasing) {
   this->arr = {1.5f, -2.0f, 3.5f, 0.0f, -1.0f, 2.0f, 4.0f, -3.0f};
   Sort<std::array<float, 8>, FloatDecreasingOrder>(this->arr.begin(),
                                                    this->arr.end());
-  EXPECT_EQ(this->ContainerToString(this->arr), "4 3.5 2 1.5 0 -1 -2 -3");
+  EXPECT_EQ(this->PrintContainer(this->arr), "4 3.5 2 1.5 0 -1 -2 -3");
 }
 
 TEST_F(FloatSortTest, FloatVectorSortDecreasing) {
   this->vec = {2.5f, -0.5f, 1.0f, 4.0f, -2.0f, 3.0f, -1.5f, 0.0f};
   Sort<std::vector<float>, FloatDecreasingOrder>(this->vec.begin(),
                                                  this->vec.end());
-  EXPECT_EQ(this->ContainerToString(this->vec), "4 3 2.5 1 0 -0.5 -1.5 -2");
+  EXPECT_EQ(this->PrintContainer(this->vec), "4 3 2.5 1 0 -0.5 -1.5 -2");
 }
 
 TEST_F(FloatSortTest, FloatListSortDecreasing) {
   this->lst = {-3.0f, 2.0f, 0.0f, 4.0f, -1.0f, 3.5f, -2.5f, 1.5f};
   Sort<std::list<float>, FloatDecreasingOrder>(this->lst.begin(),
                                                this->lst.end());
-  EXPECT_EQ(this->ContainerToString(this->lst), "4 3.5 2 1.5 0 -1 -2.5 -3");
+  EXPECT_EQ(this->PrintContainer(this->lst), "4 3.5 2 1.5 0 -1 -2.5 -3");
 }
 
 TEST_F(FloatSortTest, FloatDequeSortDecreasing) {
   this->deq = {0.0f, -1.0f, 2.0f, 5.0f, -2.0f, 3.0f, 1.0f, 4.0f};
   Sort<std::deque<float>, FloatDecreasingOrder>(this->deq.begin(),
                                                 this->deq.end());
-  EXPECT_EQ(this->ContainerToString(this->deq), "5 4 3 2 1 0 -1 -2");
+  EXPECT_EQ(this->PrintContainer(this->deq), "5 4 3 2 1 0 -1 -2");
 }
 
 using CharSortTest = SortTest<char>;
@@ -187,26 +187,64 @@ TEST_F(CharSortTest, CharArrSortIncreasing) {
   this->arr = {'d', 'a', 'c', 'b', 'h', 'e', 'g', 'f'};
   Sort<std::array<char, 8>, CharIncreasingOrder>(this->arr.begin(),
                                                  this->arr.end());
-  EXPECT_EQ(this->ContainerToString(this->arr), "a b c d e f g h");
+  EXPECT_EQ(this->PrintContainer(this->arr), "a b c d e f g h");
 }
 
 TEST_F(CharSortTest, CharVectorSortIncreasing) {
   this->vec = {'z', 'x', 'y', 'w', 'v', 'u', 't', 's'};
   Sort<std::vector<char>, CharIncreasingOrder>(this->vec.begin(),
                                                this->vec.end());
-  EXPECT_EQ(this->ContainerToString(this->vec), "s t u v w x y z");
+  EXPECT_EQ(this->PrintContainer(this->vec), "s t u v w x y z");
 }
 
 TEST_F(CharSortTest, CharListSortIncreasing) {
   this->lst = {'k', 'm', 'l', 'j', 'i', 'n', 'h', 'o'};
   Sort<std::list<char>, CharIncreasingOrder>(this->lst.begin(),
                                              this->lst.end());
-  EXPECT_EQ(this->ContainerToString(this->lst), "h i j k l m n o");
+  EXPECT_EQ(this->PrintContainer(this->lst), "h i j k l m n o");
 }
 
 TEST_F(CharSortTest, CharDequeSortIncreasing) {
   this->deq = {'c', 'b', 'd', 'a', 'h', 'g', 'f', 'e'};
   Sort<std::deque<char>, CharIncreasingOrder>(this->deq.begin(),
                                               this->deq.end());
-  EXPECT_EQ(this->ContainerToString(this->deq), "a b c d e f g h");
+  EXPECT_EQ(this->PrintContainer(this->deq), "a b c d e f g h");
+}
+
+using StringSortTest = SortTest<std::string>;
+
+TEST_F(StringSortTest, StringArrSortLengthDecreasing) {
+  this->arr = {"gg3",      "aaaaaaaa9", "ddddd6",  "h2",
+               "bbbbbbb8", "fff4",      "cccccc7", "eeee5"};
+  Sort<std::array<std::string, 8>, StringLengthDecreasingOrder>(
+      this->arr.begin(), this->arr.end());
+  EXPECT_EQ(this->PrintContainer(this->arr),
+            "aaaaaaaa9 bbbbbbb8 cccccc7 ddddd6 eeee5 fff4 gg3 h2");
+}
+
+TEST_F(StringSortTest, StringVectorSortLengthDecreasing) {
+  this->vec = {"xx3",  "yyyyyyyy9", "z2",      "kkkkk6",
+               "mmm4", "nnnn5",     "pppppp7", "qqqqqqq8"};
+  Sort<std::vector<std::string>, StringLengthDecreasingOrder>(this->vec.begin(),
+                                                              this->vec.end());
+  EXPECT_EQ(this->PrintContainer(this->vec),
+            "yyyyyyyy9 qqqqqqq8 pppppp7 kkkkk6 nnnn5 mmm4 xx3 z2");
+}
+
+TEST_F(StringSortTest, StringListSortLengthDecreasing) {
+  this->lst = {"alpha6",    "b2",   "charlie8", "dd3",
+               "echoecho9", "foo4", "gogo5",    "fwouhu7"};
+  Sort<std::list<std::string>, StringLengthDecreasingOrder>(this->lst.begin(),
+                                                            this->lst.end());
+  EXPECT_EQ(this->PrintContainer(this->lst),
+            "echoecho9 charlie8 fwouhu7 alpha6 gogo5 foo4 dd3 b2");
+}
+
+TEST_F(StringSortTest, StringDequeSortLengthDecreasing) {
+  this->deq = {"sun4",   "moonlight10", "star5", "galaxywide11",
+               "cosmo6", "planet7",     "io3",   "nebulae8"};
+  Sort<std::deque<std::string>, StringLengthDecreasingOrder>(this->deq.begin(),
+                                                             this->deq.end());
+  EXPECT_EQ(this->PrintContainer(this->deq),
+            "galaxywide11 moonlight10 nebulae8 planet7 cosmo6 star5 sun4 io3");
 }
